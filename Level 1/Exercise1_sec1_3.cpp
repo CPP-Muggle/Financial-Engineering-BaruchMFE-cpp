58e120d4c60af5a16891cfa15773f36997a3f906
ShihanUTSA <shihanutb@gmail.com>
2016-10-19 13:38:39 -0500

    Update Exercise1_sec1_3.cpp

--- Level 1/Exercise1_sec1_3.cpp
+++ Level 1/Exercise1_sec1_3.cpp
@@ -4,9 +4,6 @@ using namespace std;
 int main()
 {
 	printf("My first C-program\nis a fact!\nGood, isn't it?");
-	//printf("My first C-program\n");
-	//printf("is a fact!\n");
-	//printf("Good, isn't it?\n");
 	getchar(); // I used this function to keep console on the screen.
     return 0;
 
